2c9c92c43df25249b50978eb6ef208d54c6f2a6f
malak-wahyb <malakwahyb537@gmail.com>
2023-05-31 22:49:58 +0300

    bddiqwjbahefcvbweabfcf

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   console.log(err)
-  res.status(err.status || 500).send({
+  res.status(500).send({
     success: false,
     message: err.message,
   });
